64dbfa71e9bad6115e457e4b2d5ec0e13873d06e
Gwenael Treguier <gwen@cas75-5-78-192-41-37.fbxo.proxad.net>
2015-09-11 21:44:37 +0200

    Upgrade to Hibernate 5.

--- src/main/java/org/hibernate/dialect/SQLiteDialect.java
+++ src/main/java/org/hibernate/dialect/SQLiteDialect.java
@@ -13,39 +13,53 @@ import java.sql.SQLException;
 import java.sql.Types;
 
 import org.hibernate.JDBCException;
+import org.hibernate.ScrollMode;
 import org.hibernate.dialect.function.AbstractAnsiTrimEmulationFunction;
 import org.hibernate.dialect.function.NoArgSQLFunction;
 import org.hibernate.dialect.function.SQLFunction;
 import org.hibernate.dialect.function.SQLFunctionTemplate;
 import org.hibernate.dialect.function.StandardSQLFunction;
 import org.hibernate.dialect.function.VarArgsSQLFunction;
+import org.hibernate.dialect.pagination.AbstractLimitHandler;
+import org.hibernate.dialect.pagination.LimitHandler;
+import org.hibernate.dialect.pagination.LimitHelper;
+import org.hibernate.dialect.unique.DefaultUniqueDelegate;
+import org.hibernate.dialect.unique.UniqueDelegate;
+import org.hibernate.engine.spi.RowSelection;
 import org.hibernate.exception.*;
+import org.hibernate.exception.spi.SQLExceptionConversionDelegate;
+import org.hibernate.exception.spi.TemplatedViolatedConstraintNameExtracter;
+import org.hibernate.exception.spi.ViolatedConstraintNameExtracter;
+import org.hibernate.internal.util.JdbcExceptionHelper;
+import org.hibernate.mapping.Column;
 import org.hibernate.type.StandardBasicTypes;
 
 public class SQLiteDialect extends Dialect {
+  private final UniqueDelegate uniqueDelegate;
+
   public SQLiteDialect() {
     registerColumnType(Types.BIT, "boolean");
-    registerColumnType(Types.TINYINT, "tinyint");
-    registerColumnType(Types.SMALLINT, "smallint");
-    registerColumnType(Types.INTEGER, "integer");
-    registerColumnType(Types.BIGINT, "bigint");
-    registerColumnType(Types.FLOAT, "float");
-    registerColumnType(Types.REAL, "real");
-    registerColumnType(Types.DOUBLE, "double");
-    registerColumnType(Types.NUMERIC, "numeric($p, $s)");
+    //registerColumnType(Types.TINYINT, "tinyint");
+    //registerColumnType(Types.SMALLINT, "smallint");
+    //registerColumnType(Types.INTEGER, "integer");
+    //registerColumnType(Types.BIGINT, "bigint");
+    //registerColumnType(Types.FLOAT, "float");
+    //registerColumnType(Types.REAL, "real");
+    //registerColumnType(Types.DOUBLE, "double");
+    //registerColumnType(Types.NUMERIC, "numeric($p, $s)");
     registerColumnType(Types.DECIMAL, "decimal");
     registerColumnType(Types.CHAR, "char");
     registerColumnType(Types.VARCHAR, "varchar($l)");
     registerColumnType(Types.LONGVARCHAR, "longvarchar");
-    registerColumnType(Types.DATE, "date");
-    registerColumnType(Types.TIME, "time");
+    //registerColumnType(Types.DATE, "date");
+    //registerColumnType(Types.TIME, "time");
     registerColumnType(Types.TIMESTAMP, "datetime");
     registerColumnType(Types.BINARY, "blob");
     registerColumnType(Types.VARBINARY, "blob");
     registerColumnType(Types.LONGVARBINARY, "blob");
-    registerColumnType(Types.BLOB, "blob");
-    registerColumnType(Types.CLOB, "clob");
-    registerColumnType(Types.BOOLEAN, "boolean");
+    //registerColumnType(Types.BLOB, "blob");
+    //registerColumnType(Types.CLOB, "clob");
+    //registerColumnType(Types.BOOLEAN, "boolean");
 
     registerFunction( "concat", new VarArgsSQLFunction(StandardBasicTypes.STRING, "", "||", "") );
     registerFunction( "mod", new SQLFunctionTemplate(StandardBasicTypes.INTEGER, "?1 % ?2" ) );
@@ -82,7 +96,17 @@ public class SQLiteDialect extends Dialect {
           return new SQLFunctionTemplate(StandardBasicTypes.STRING, "rtrim(?1, ?2)");
         }
     } );
+    uniqueDelegate = new SQLiteUniqueDelegate(this);
   }
+
+  // database type mapping support ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+  @Override
+  public String getCastTypeName(int code) {
+    return super.getCastTypeName(code); // FIXME
+  }
+
+  // IDENTITY support ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
   @Override
   public boolean supportsIdentityColumns() {
@@ -120,34 +144,44 @@ public class SQLiteDialect extends Dialect {
     return "select last_insert_rowid()";
   }
 
+  // limit/offset support ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+  private static final AbstractLimitHandler LIMIT_HANDLER = new AbstractLimitHandler() {
+    @Override
+    public String processSql(String sql, RowSelection selection) {
+      final boolean hasOffset = LimitHelper.hasFirstRow( selection );
+      return sql + (hasOffset ? " limit ? offset ?" : " limit ?");
+    }
+
+    @Override
+    public boolean supportsLimit() {
+      return true;
+    }
+
+    @Override
+    public boolean bindLimitParametersInReverseOrder() {
+      return true;
+    }
+  };
+
   @Override
-  public boolean supportsLimit() {
-    return true;
+  public LimitHandler getLimitHandler() {
+    return LIMIT_HANDLER;
+  }
+
+  // lock acquisition support ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+  @Override
+  public boolean supportsLockTimeouts() {
+    return false; // may be http://sqlite.org/c3ref/db_mutex.html ?
   }
 
   @Override
-  public boolean bindLimitParametersInReverseOrder() {
-    return true;
+  public String getForUpdateString() {
+    return "";
   }
 
   @Override
-  protected String getLimitString(String query, boolean hasOffset) {
-    return query + (hasOffset ? " limit ? offset ?" : " limit ?");
-  }
-
-  @Override
-  public boolean supportsTemporaryTables() {
-    return true;
-  }
-
-  @Override
-  public String getCreateTemporaryTableString() {
-    return "create temporary table if not exists";
-  }
-
-  @Override
-  public Boolean performTemporaryTableDDLInIsolation() {
-    return Boolean.FALSE;
+  public boolean supportsOuterJoinForUpdate() {
+    return false;
   }
 
   /*
@@ -157,12 +191,13 @@ public class SQLiteDialect extends Dialect {
   }
   */
 
+  // current timestamp support ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
   @Override
   public boolean supportsCurrentTimestampSelection() {
     return true;
   }
 
-  @Override
   public boolean isCurrentTimestampSelectStringCallable() {
     return false;
   }
@@ -171,6 +206,8 @@ public class SQLiteDialect extends Dialect {
   public String getCurrentTimestampSelectString() {
     return "select current_timestamp";
   }
+
+  // SQLException support ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
   private static final int SQLITE_BUSY = 5;
   private static final int SQLITE_LOCKED = 6;
@@ -184,36 +221,54 @@ public class SQLiteDialect extends Dialect {
   private static final int SQLITE_CONSTRAINT = 19;
   private static final int SQLITE_MISMATCH = 20;
   private static final int SQLITE_NOTADB = 26;
+
   @Override
-  public SQLExceptionConverter buildSQLExceptionConverter() {
-    return new SQLExceptionConverter() {
+  public SQLExceptionConversionDelegate buildSQLExceptionConversionDelegate() {
+    return new SQLExceptionConversionDelegate() {
       @Override
       public JDBCException convert(SQLException sqlException, String message, String sql) {
-        final int errorCode = JDBCExceptionHelper.extractErrorCode(sqlException);
-        if (errorCode == SQLITE_CONSTRAINT) {
-          final String constraintName = EXTRACTER.extractConstraintName(sqlException);
-          return new ConstraintViolationException(message, sqlException, sql, constraintName);
-        } else if (errorCode == SQLITE_TOOBIG || errorCode == SQLITE_MISMATCH) {
+        final int errorCode = JdbcExceptionHelper.extractErrorCode(sqlException);
+        if (errorCode == SQLITE_TOOBIG || errorCode == SQLITE_MISMATCH) {
           return new DataException(message, sqlException, sql);
         } else if (errorCode == SQLITE_BUSY || errorCode == SQLITE_LOCKED) {
           return new LockAcquisitionException(message, sqlException, sql);
         } else if ((errorCode >= SQLITE_IOERR && errorCode <= SQLITE_PROTOCOL) || errorCode == SQLITE_NOTADB) {
           return new JDBCConnectionException(message, sqlException, sql);
         }
-        return new GenericJDBCException(message, sqlException, sql);
+
+        // returning null allows other delegates to operate
+        return null;
       }
     };
   }
 
-  public static final ViolatedConstraintNameExtracter EXTRACTER = new TemplatedViolatedConstraintNameExtracter() {
-    public String extractConstraintName(SQLException sqle) {
-      return extractUsingTemplate( "constraint ", " failed", sqle.getMessage() );
+  public ViolatedConstraintNameExtracter getViolatedConstraintNameExtracter() {
+    return EXTRACTER;
+  }
+
+  private static final ViolatedConstraintNameExtracter EXTRACTER = new TemplatedViolatedConstraintNameExtracter() {
+    @Override
+    protected String doExtractConstraintName(SQLException sqle) throws NumberFormatException {
+      final int errorCode = JdbcExceptionHelper.extractErrorCode(sqle);
+      if (errorCode == SQLITE_CONSTRAINT) {
+        return extractUsingTemplate("constraint ", " failed", sqle.getMessage());
+      }
+      return null;
     }
   };
+
+  // union subclass support ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
   @Override
   public boolean supportsUnionAll() {
     return true;
+  }
+
+  // DDL support ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+  @Override
+  public boolean canCreateSchema() {
+    return false;
   }
 
   @Override
@@ -226,21 +281,16 @@ public class SQLiteDialect extends Dialect {
     return false;
   }
 
+  @Override
+  public boolean qualifyIndexName() {
+    return false;
+  }
+
   /*
   public String getAddColumnString() {
     return "add column";
   }
   */
-
-  @Override
-  public String getForUpdateString() {
-    return "";
-  }
-
-  @Override
-  public boolean supportsOuterJoinForUpdate() {
-    return false;
-  }
 
   @Override
   public String getDropForeignKeyString() {
@@ -260,15 +310,19 @@ public class SQLiteDialect extends Dialect {
   }
 
   @Override
+  public boolean supportsCommentOn() {
+    return true;
+  }
+
+  @Override
   public boolean supportsIfExistsBeforeTableName() {
     return true;
   }
 
-  /*
-  public boolean supportsCascadeDelete() {
-    return true;
+  @Override
+  public String getCascadeConstraintsString() {
+    return " cascade";
   }
-  */
 
   /* not case insensitive for unicode characters by default (ICU extension needed)
   public boolean supportsCaseInsensitiveLike() {
@@ -277,12 +331,44 @@ public class SQLiteDialect extends Dialect {
   */
 
   @Override
+  public boolean doesReadCommittedCauseWritersToBlockReaders() {
+    return true; // TODO Validate (WAL mode...)
+  }
+
+  public boolean doesRepeatableReadCauseReadersToBlockWriters() {
+    return true;
+  }
+
+  @Override
   public boolean supportsTupleDistinctCounts() {
     return false;
+  }
+
+  public int getInExpressionCountLimit() {
+    return 1000; // Compile/runtime time option: http://sqlite.org/limits.html#max_variable_number
+  }
+
+  @Override
+  public UniqueDelegate getUniqueDelegate() {
+    return uniqueDelegate;
+  }
+  private static class SQLiteUniqueDelegate extends DefaultUniqueDelegate {
+    public SQLiteUniqueDelegate(Dialect dialect) {
+      super(dialect);
+    }
+    @Override
+    public String getColumnDefinitionUniquenessFragment(Column column) {
+      return " unique";
+    }
   }
 
   @Override
   public String getSelectGUIDString() {
     return "select hex(randomblob(16))";
+  }
+
+  @Override
+  public ScrollMode defaultScrollMode() {
+    return ScrollMode.FORWARD_ONLY;
   }
 }
